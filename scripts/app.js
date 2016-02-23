62b9490e062afa34872f0d783ef21621d078a7b2
Tomer Rosenfeld <tomerosenfeld007@gmail.com>
2016-02-23 16:19:54 +0200

    Finished

--- scripts/app.js
+++ scripts/app.js
@@ -8,7 +8,7 @@ var app = angular.module('Landing', ['ngMaterial']).config(function ($mdThemingP
 
 });
 app.controller('MainController', function ($scope, $rootScope, $http, $mdMedia) {
-    $scope.current_answers = ["I love gaming on my phone", "I love watching media on my phone", "I love texting on my phone", "I love to read on my phone"]
+
     $scope.allAnswers = [
         [
             "I love gaming on my phone",
@@ -19,11 +19,29 @@ app.controller('MainController', function ($scope, $rootScope, $http, $mdMedia) 
             "I want a big screen",
             "I want a small screen",
             "I want an average sized screen"
+        ],
+        [
+            "I want my battery to last all day long",
+            "I want an average sized battery",
+            "I don't care about my battery"
+        ],
+        [
+            "I take a lot of pictures and videos",
+            "I take pictures and videos now and then",
+            "I don't care about the camera on my phone"
         ]
     ];
+    $scope.current_answers = $scope.allAnswers[0];
     $scope.current = 0;
     $scope.changeCurrent = function () {
         $scope.current++;
-        $scope.current_answers = $scope.current_answers = $scope.allAnswers[$scope.current];
-    }
+        if($scope.current>=$scope.allAnswers.length){
+            open("GetANexus.html")
+        }
+        else {
+            $scope.current_answers = $scope.current_answers = $scope.allAnswers[$scope.current];
+        }
+    };
+    $scope.url = "https://www.google.com/nexus/";
+    $scope.imgURL = "";
 });
